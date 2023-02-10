3740031450c086731f874e512d3e728b87442c5a
oluwasinaoseniwaya <98983612+oluwasinaoseniwaya@users.noreply.github.com>
2023-02-10 10:54:32 +0100

    fix: live url

--- wayapaylib/src/main/java/com/faymosInc/wayapaylib/constants/Enums.java
+++ wayapaylib/src/main/java/com/faymosInc/wayapaylib/constants/Enums.java
@@ -1,13 +1,13 @@
 package com.faymosInc.wayapaylib.constants;
 
 public class Enums {
-    private final static String BASE_URL = "https://services.staging.wayapay.ng";
+    private final static String BASE_URL = "https://services.wayaquick.com";
 
     public static final String testUrlPayment = BASE_URL + "/payment-gateway/api/v1/request/transaction";
     public static final String liveUrlPayment = BASE_URL + "";
     public static final String testUrlQuery =BASE_URL + "/payment-gateway/api/v1/reference/query/";
     public static final String liveUrlQuery =BASE_URL + "";
-    public static final String aurUrlTest = "https://pay.staging.wayapay.ng/?_tranId=";
-    public static final String aurUrlLive = "https://pay.staging.wayapay.ng/?_tranId=";
+    public static final String aurUrlTest = "https://pay.wayaquick.com/?_tranId=";
+    public static final String aurUrlLive = "https://pay.wayaquick.com/?_tranId=";
 
 }
